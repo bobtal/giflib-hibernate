c55c878a57f4fbfa85d2c18c346e9b9ddd011bdc
edison696 <talevski.boban@gmail.com>
2017-11-25 14:46:31 +0100

    end of section 4

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -3,18 +3,22 @@ package com.teamtreehouse.giflib.web.controller;
 import com.teamtreehouse.giflib.model.Category;
 import com.teamtreehouse.giflib.service.CategoryService;
 import com.teamtreehouse.giflib.web.Color;
+import com.teamtreehouse.giflib.web.FlashMessage;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,7 +51,9 @@ public class CategoryController {
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
         // TODO: Add model attributes needed for new form
-        model.addAttribute("category", new Category());
+        if(!model.containsAttribute("category")) {
+            model.addAttribute("category", new Category());
+        }
         model.addAttribute("colors", Color.values());
         return "category/form";
     }
@@ -71,10 +77,23 @@ public class CategoryController {
 
     // Add a category
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(Category category) {
+    public String addCategory(@Valid Category category, BindingResult result, RedirectAttributes redirectAttributes) {
         // TODO: Add category if valid data was received
+        if (result.hasErrors()) {
+            // Include validations errors upon redirect
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.category", result);
+
+            // Add the category object if invalid data was received
+            // so that we can have that object available after the redirect
+            // and preserve the invalid data the user has entered
+            redirectAttributes.addFlashAttribute("category", category);
+            // Redirect back to the form
+            return "redirect:/categories/add";
+        }
         categoryService.save(category);
 
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage("Category successfully added", FlashMessage.Status.SUCCESS));
         // TODO: Redirect browser to /categories
         return "redirect:/categories";
     }
